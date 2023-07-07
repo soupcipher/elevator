d409e7cb3a1364674b2a012739698b7acf5944ff
Alyssa Feagans <alyssafeagans@gmail.com>
2023-07-06 23:35:06 -0600

    Adding more tests and updating read me

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
     Returns a string in format = "TOTAL_TRAVEL_TIME FLOOR, FLOOR, FLOOR"
 
     Script ignores duplicate floors back to back (can only visit a floor once without leaving)
+    Assumes elevator has "infinite" positive and negative floors and there are no gaps in numbers in the elevator.
+    Infinite being restricted to the available memory on the system being used to run.
 
     Dependencies: python >= 3.0
     Runtime: O(n) where n is length of input array (num of floors to visit)
@@ -24,10 +26,13 @@
     To run all tests:
     $ pytest python/test_elevator.py
 
+    To get more info on a test:     
+    $ pytest python/test_elevator.py -vv
+
 ### TODOS:
 - ~~Python - return output instead of printing~~
 - Python - allow user to input example through command line
-- Write tests for python solution
+- ~~Write tests for python solution~~
 - Write a solution as a bash script
 - Write a solution in Java
 - Write a solution in C
--- python/test_elevator.py
+++ python/test_elevator.py
@@ -17,3 +17,27 @@ def test_repeat_floors():
 
 def test_consecutive():
     assert elevator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]) == "140 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15"
+
+def test_negative():
+    assert elevator([-4, 4]) == "80 -4, 4"
+
+def test_ground_to_basement():
+    assert elevator([0, -3]) == "30 0, -3"
+
+def test_superstitous():
+    assert elevator([1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17]) == "160 1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17"
+
+def test_china():
+    assert elevator([-1, 2, 3, 5, 10, 13]) == "140 -1, 2, 3, 5, 10, 13"
+
+# Floors 1 to 99
+def test_elevator_to_sky():
+    long_list = list(range(1, 100))
+    expected = "980 " + ", ".join(str(num) for num in long_list)
+    assert elevator(long_list) == expected
+
+# Floors 1 to 9999
+def test_elevator_to_heaven():
+    long_list = list(range(1, 10000))
+    expected = "99980 " + ", ".join(str(num) for num in long_list)
+    assert elevator(long_list) == expected
